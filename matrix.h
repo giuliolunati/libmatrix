a9560cd83e93fd0a50b38b75ac38f9fb512f00e8
Giulio Lunati <giuliolunati@gmail.com>
2016-09-20 21:39:30 +0200

    Function prototypes in matrix.h

--- matrix.h
+++ matrix.h
@@ -1,5 +1,9 @@
+//// GENERAL STUFF ////
 #define REAL float
 #define uint unsigned int
+void error(char *msg);
+
+//// MATRIX ////
 typedef struct {
   uint height;
   uint width;
@@ -9,5 +13,14 @@ typedef struct {
   uint dx;
   uint dy;
 } matrix;
+// basic //
+matrix *matrix_init(uint height, uint width);
+matrix *matrix_free(matrix *m);
+matrix *matrix_check(matrix *m);
+matrix *matrix_set_width(matrix *m, uint width);
+REAL matrix_get(matrix *self, uint c, uint r);
+void matrix_set(matrix *self, uint c, uint r, REAL v);
+// input/output //
 matrix *matrix_fscanf(FILE * f);
 void matrix_fprintf(FILE *f, matrix *self);
+void matrix_printf(matrix *self);
